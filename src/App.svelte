3db51db05aa88f2c18f0edbd40112a980410db0d
Yuval Allweil <themarquisdesheric@gmail.com>
2021-10-23 21:06:12 -0700

    basic game board layout in place

--- src/App.svelte
+++ src/App.svelte
@@ -2,20 +2,19 @@
 	type Row = string[]
 	
 	const rows: [Row, Row, Row, Row, Row] = [
-		['x', 'x', 'x', 'x', 'x'], 
-		['x', 'x', 'x', 'x', 'x'], 
-		['x', 'x', 'x', 'x', 'x'], 
-		['x', 'x', 'x', 'x', 'x'], 
-		['x', 'x', 'x', 'x', 'x']
+		['x', '', 'x', 'x', 'x'], 
+		['x', 'x', '', 'x', 'x'], 
+		['x', 'x', 'x', '', 'x'], 
+		['', 'x', 'x', 'x', ''], 
+		['x', '', 'x', '', 'x']
 	]
 </script>
 
-<main>
-	<h1>The Game of Life</h1>
-	<div class="game-board">
+<main class="h-screen flex justify-center items-center text-center">
+	<div class="game-board grid grid-rows-5 grid-cols-5 gap-x-px gap-y-px">
 		{#each rows as row}
 			{#each row as square}
-				<span class="square">{square}</span>
+				<span class="square w-12 h-12" class:filled={square} />
 			{/each}
 		{/each}
 	</div>
@@ -24,26 +23,20 @@
 <style global>
   @tailwind base;
   @tailwind utilities;
-	
-	main {
-		width: fit-content;
-		text-align: center;
-		padding: 1em;
-		margin: 0 auto;
-	}
 
+	:global(body) {
+		background-color: #aaa;
+	}
+	
 	.game-board {
-		display: grid;
-		grid-template-rows: repeat(3, 1fr);
-		grid-template-columns: repeat(5, 1fr);
+		width: fit-content;
 	}
 
 	.square {
-		border: 1px solid;
-		margin-right: -1px;
-		margin-top: -1px;
-		width: 50px;
-		height: 50px;
-		line-height: 50px;;
+		line-height: 3rem;
+	}
+
+	.square.filled {
+		background-color: #000;
 	}
 </style>
