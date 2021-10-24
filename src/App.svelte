b3fa06a40223c17b4f0caa937bb61695880840b3
Yuval Allweil <themarquisdesheric@gmail.com>
2021-10-23 20:07:13 -0700

    install tailwindCSS

--- src/App.svelte
+++ src/App.svelte
@@ -21,7 +21,10 @@
 	</div>
 </main>
 
-<style>
+<style global>
+  @tailwind base;
+  @tailwind utilities;
+	
 	main {
 		width: fit-content;
 		text-align: center;
@@ -31,14 +34,14 @@
 
 	.game-board {
 		display: grid;
-    grid-template-rows: repeat(3, 1fr);
-    grid-template-columns: repeat(5, 1fr);
+		grid-template-rows: repeat(3, 1fr);
+		grid-template-columns: repeat(5, 1fr);
 	}
 
 	.square {
 		border: 1px solid;
 		margin-right: -1px;
-    margin-top: -1px;
+		margin-top: -1px;
 		width: 50px;
 		height: 50px;
 		line-height: 50px;;
