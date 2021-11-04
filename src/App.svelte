bc825ae54a5870c7dc007e3803820c6e8ab0f1bf
Yuval Allweil <themarquisdesheric@gmail.com>
2021-11-03 23:42:21 -0700

    lightly style App and create Button component

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { onDestroy } from 'svelte'
 	import { interval } from './stores'
+	import Button from './components/Button.svelte'
 	import { createGameBoard, updateGameBoard, isEvolutionOver } from './utils'
 	import type { GameBoard } from './global'
 	import { Emojis } from './enums'
@@ -52,14 +53,14 @@
 	onDestroy(() => clearInterval($interval))
 </script>
 
-<main class="h-screen flex flex-col justify-center items-center text-center">
-	<div class="game-board grid grid-rows-{BOARD_LENGTH} grid-cols-{BOARD_LENGTH} gap-x-px gap-y-px">
+<main class="h-screen flex flex-col justify-center items-center text-center bg-gray-800">
+	<div class="game-board grid grid-rows-{BOARD_LENGTH} grid-cols-{BOARD_LENGTH} gap-x-px gap-y-px rounded-3xl p-12 bg-gray-200">
 		{#each gameBoard as row}
 			{#each row as cell}
 				<span
 					class="cell w-12 h-12 text-5xl"
-					class:opacity-25={cell.emoji === Emojis.dead}
-					>
+					class:opacity-50={cell.emoji === Emojis.dead}
+				>
 					{#if cell.emoji}
 						{cell.emoji}
 					{/if}
@@ -68,7 +69,7 @@
 		{/each}
 	</div>
 
-	<p class="mt-8" class:font-bold={evolutionOver}>
+	<p class="mt-8 text-gray-200" class:font-bold={evolutionOver}>
 		{evolutionOver ? 'This experiment survived ' : ''}
 		{generations} generation{generations > 1 ? 's' : ''}
 	</p>
@@ -76,27 +77,18 @@
 	<div>
 		{#if !evolutionOver}
 			{#if evolutionPaused}
-				<button
-					on:click={startEvolution}
-					class="mt-8 mr-2 p-4 border rounded-2xl"
-				>
+				<Button onClick={startEvolution}>
 					start evolution
-				</button>
+				</Button>
 			{:else}
-				<button
-					on:click={pauseEvolution}
-					class="mt-8 mr-2 p-4 border rounded-2xl"
-				>
+				<Button onClick={pauseEvolution}>
 					stop evolution
-				</button>
+				</Button>
 			{/if}
 		{/if}
-		<button
-			on:click={replayEvolution}
-			class="mt-8 p-4 border rounded-2xl"
-		>
+		<Button onClick={replayEvolution} classes="mr-0">
 			replay evolution
-		</button>
+		</Button>
 	</div>
 </main>
 
---
+++ src/components/Button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let onClick: () => void
+  export let classes = 'mr-2'
+</script>
+
+<button
+  on:click={onClick}
+  class="mt-8 p-4 border rounded-2xl bg-gray-200 {classes}"
+>
+  <slot></slot>
+</button>
