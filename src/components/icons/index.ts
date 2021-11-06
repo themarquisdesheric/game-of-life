8c78633fef44d8535fd6c4f61831b4e5d4acb965
Yuval Allweil <themarquisdesheric@gmail.com>
2021-11-06 10:24:48 -0700

    create index file for icons

---
+++ src/components/icons/index.ts
@@ -0,0 +1,11 @@
+import PlayIcon from './PlayIcon.svelte'
+import PauseIcon from './PauseIcon.svelte'
+import RestartIcon from './RestartIcon.svelte'
+import NewIcon from './NewIcon.svelte'
+
+export {
+  PlayIcon,
+  PauseIcon,
+  RestartIcon,
+  NewIcon,
+}
